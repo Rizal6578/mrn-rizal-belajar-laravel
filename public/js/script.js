def3008afa330fdab9ff691a34714225b1d0827b
Rizal6578 <mrizalnurrudin2018@gmail.com>
2023-11-30 14:50:07 +0700

    initial commit

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+// public/js/script.js
+
+// Contoh skrip JavaScript sederhana
+document.addEventListener('DOMContentLoaded', function () {
+    // Tambahkan skrip JavaScript sesuai kebutuhan
+    console.log('Hello from script.js!');
+});
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+    <link rel="stylesheet" href="{{ asset('public/css/style.css') }}">
+</head>
+<body>
+    <div class="container">
+        <h1>Hello World</h1>
+        <p>Nama: Muhamad Rizal Nurruddin</p>
+        <p>Hoby: Memancing dan Berburu Ubur-ubur</p>
+        <p>Domisili : Demak, Jawa Tengah</p>
+    </div>
+    <script src="{{ asset('public/js/script.js') }}"></script>
+</body>
+</html>
